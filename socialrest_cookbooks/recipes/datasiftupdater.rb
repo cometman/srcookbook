59639254bbd649c34a2b1f0d94879b41a4383c00
cometman <csbuiss@gmail.com>
2015-02-11 02:56:01 -0600

    WIP

--- socialrest_cookbooks/recipes/datasiftupdater.rb
+++ socialrest_cookbooks/recipes/datasiftupdater.rb
@@ -2,6 +2,5 @@ cron "datasiftupdater" do
   hour "*"
   minute "*"
   weekday "*"
-  command "bundle exec rake job:datasift_filter_updater"
-  user "webapp"
+  command "su -c 'bundle exec rake job:datasift_filter_updater > /var/log/cron 2>&1' webapp"
 end
--- socialrest_cookbooks/recipes/sidekiq.rb
+++ socialrest_cookbooks/recipes/sidekiq.rb
@@ -2,6 +2,5 @@ cron "sidekiq" do
   hour "*"
   minute "0,10,20,30,40,50"
   weekday "*"
-  command "cd /var/app/current && ./script/sidekiq_start.sh"
-  user "webapp"
+  command "su -c 'cd /var/app/current && ./script/sidekiq_start.sh > /var/log/cron 2>&1' webapp"
 end
