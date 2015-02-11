7df6414fb54003ef98c807cbfceb090bec85d726
cometman <csbuiss@gmail.com>
2015-02-11 03:01:00 -0600

    WIP

--- socialrest_cookbooks/recipes/datasiftupdater.rb
+++ socialrest_cookbooks/recipes/datasiftupdater.rb
@@ -2,5 +2,5 @@ cron "datasiftupdater" do
   hour "*"
   minute "*"
   weekday "*"
-  command "su -c 'bundle exec rake job:datasift_filter_updater > /var/log/cron 2>&1' webapp"
+  command "su -c 'cd /var/app/current && bundle exec rake job:datasift_filter_updater > /var/log/cron 2>&1' webapp"
 end
